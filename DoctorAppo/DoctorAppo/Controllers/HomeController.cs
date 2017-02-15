b5852cf580bea90d032a45e4d383cf7fdd46bd63
SangeethaMurugesan <mmsangeetha@yahoo.com>
2017-02-15 21:42:00 +0530

    Add files via upload

---
+++ DoctorAppo/DoctorAppo/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using DoctorAppo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoctorAppo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Logi c)
+        {
+            doctorappoEntities4 db = new doctorappoEntities4();
+
+            if (true)
+            {
+                var item = db.Logis.Where(model => model.UserID.Equals(c.UserID) && model.Pwd.Equals(c.Pwd) && model.UserRole.Equals(c.UserRole)).FirstOrDefault();
+                if (item is Logi)
+                {
+                    Session["UserName"] = item.UserID;
+                    if(item.UserRole=="admin")
+                    {
+                        return RedirectToAction("Admin");
+                    }
+                    else if(item.UserRole=="receptionist")
+                    {
+                        return RedirectToAction("Receptionist");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Doctor");
+                    }
+                    
+                }
+                else
+                {
+                    return View(c);
+                }
+            }
+            //else
+            //    return View(c);
+
+        }​
+    }
+}
---
+++ DoctorAppo/DoctorAppo/Controllers/tblPatientDetailsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoctorAppo.Models
+{
+    public class tblPatientDetailsController : Controller
+    {
+        private doctorappoEntities4 db = new doctorappoEntities4();
+
+        // GET: tblPatientDetails
+        public ActionResult Index()
+        {
+            return View(db.tblPatientDetails.ToList());
+        }
+
+        // GET: tblPatientDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblPatientDetail tblPatientDetail = db.tblPatientDetails.Find(id);
+            if (tblPatientDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblPatientDetail);
+        }
+
+        // GET: tblPatientDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tblPatientDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "iId,cName,cUserName,vPassword,vAddress,dDateOfBirth,cGender,vEmail,iPhone,Security_Question,Answer")] tblPatientDetail tblPatientDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblPatientDetails.Add(tblPatientDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblPatientDetail);
+        }
+
+        // GET: tblPatientDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblPatientDetail tblPatientDetail = db.tblPatientDetails.Find(id);
+            if (tblPatientDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblPatientDetail);
+        }
+
+        // POST: tblPatientDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "iId,cName,cUserName,vPassword,vAddress,dDateOfBirth,cGender,vEmail,iPhone,Security_Question,Answer")] tblPatientDetail tblPatientDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblPatientDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblPatientDetail);
+        }
+
+        // GET: tblPatientDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblPatientDetail tblPatientDetail = db.tblPatientDetails.Find(id);
+            if (tblPatientDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblPatientDetail);
+        }
+
+        // POST: tblPatientDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblPatientDetail tblPatientDetail = db.tblPatientDetails.Find(id);
+            db.tblPatientDetails.Remove(tblPatientDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
